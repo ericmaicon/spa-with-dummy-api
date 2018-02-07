5c6f19a78d2a24769ef67bb2b4ee967fe8bda76a
Eric Maicon <eric@ericmaicon.com.br>
2018-02-07 20:25:20 -0200

    initial react structure

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, '/src/index.js'),
+  resolve: {
+    extensions: [ '.js', '.jsx' ],
+    modules: [
+      path.resolve('./src'),
+      path.resolve('./node_modules')
+    ]
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.join(__dirname, 'src'),
+        loaders: ['babel-loader'],
+        exclude: [
+          path.resolve(__dirname, 'node_modules')
+        ]
+      }
+    ]
+  }
+}
