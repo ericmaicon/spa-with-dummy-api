a4f95bec094c4d1046c1d1153d9d4d92488e772a
Eric Maicon <eric@ericmaicon.com.br>
2018-02-08 00:26:02 -0200

    fix package scripts

--- server.js
+++ server.js
@@ -4,7 +4,4 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, './dist/')));
-app.use((request, response) => {
-  response.redirect('/');
-});
-app.listen(3333, () => console.log('Running on 0.0.0.0:3333'));
+app.listen(3333, () => console.log('App READY and Running on 0.0.0.0:3333'));
